92bcc7969d5010ed89c45f9cad2e44184e9eb478
Enzo Doyen <spidersouris@gmail.com>
2024-02-05 15:13:40 +0100

    refactor(website): reworked language icon overriding

--- website/phugo.sh
+++ website/phugo.sh
@@ -3,21 +3,3 @@ hugo
 
 
 rsync -avh public/ site/ --delete
-
-sed -i.bkp 's/class="fas fa-globe mr-1"/class="fa fa-language"/g' site/index.html
-sed -i.bkp 's/class="fas fa-globe mr-1"/class="fa fa-language"/g' site/fr/index.html
-
-sed -i.bkp 's/class="fas fa-globe mr-1"/class="fa fa-language"/g' site/events/data-and-theater/index.html
-sed -i.bkp 's/class="fas fa-globe mr-1"/class="fa fa-language"/g' site/fr/evenements/donnees-de-theatre/index.html
-
-sed -i.bkp 's/class="fas fa-globe mr-1"/class="fa fa-language"/g' site/events/index.html
-sed -i.bkp 's/class="fas fa-globe mr-1"/class="fa fa-language"/g' site/fr/evenements/index.html
-
-sed -i.bkp 's/class="fas fa-globe mr-1"/class="fa fa-language"/g' site/post/index.html
-sed -i.bkp 's/class="fas fa-globe mr-1"/class="fa fa-language"/g' site/fr/post/index.html
-
-sed -i.bkp 's/class="fas fa-globe mr-1"/class="fa fa-language"/g' site/post/23-10-15-fete-de-la-science/index.html
-sed -i.bkp 's/class="fas fa-globe mr-1"/class="fa fa-language"/g' site/fr/post/23-10-15-fete-de-la-science/index.html
-
-sed -i.bkp 's/class="fas fa-globe mr-1"/class="fa fa-language"/g' site/post/2023-06-28-poster-humanistica/index.html
-sed -i.bkp 's/class="fas fa-globe mr-1"/class="fa fa-language"/g' site/fr/post/2023-06-28-poster-humanistica/index.html
